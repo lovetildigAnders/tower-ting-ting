a1ae33214b96a2a09c7a102d56978aba021b6b81
lovetildigAnders <asylvesterp@gmail.com>
2021-11-08 14:26:45 +0100

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=b8e6bbb777b72703c4c1&state=bfccc0e9-6e70-4f7e-9df9-18d94f16d916

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace tower_ting_ting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int number;
+         bool isParsable =Int32.TryParse(args[0],out number);
+         if(isParsable)
+         hannoi(number,"A","B","C");
+         else
+         {
+             Console.WriteLine("use a whole number");
+         }
+         void move(string f,string t)
+         {
+             Console.WriteLine($"move dsci from {f} to {t}!");
+         }
+         void hannoi(int n, string f, string h,string t)
+         {
+             if(n ==0)
+             {
+                 return;
+             }
+             else{
+                 hannoi(n-1,f,t,h);
+                 move(f,t);
+                 hannoi(n-1,h,f,t);
+             }
+         }
+
+        }
+    }
+}
